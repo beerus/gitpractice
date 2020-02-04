21f95df381da66beaec7d1275ac19c384190ad1c
Rajual Zaki <rajualzaki@gmail.com>
2020-02-04 03:44:02 -0500

    ugh

--- divide.js
+++ divide.js
@@ -1,9 +1,5 @@
 // divide.js
 <<<<<<< HEAD
 function modulus(a, b) {
-    return a % b;
-=======
-function divide(a, b) {
-    // TODO return
->>>>>>> f60dd1e94defb2f01ea2fe22a1dada561c6d8522
+    return a % b; 
 };
--- subtract.js
+++ subtract.js
@@ -2,9 +2,4 @@
 // subtract.js// return subtraction of two numbers
 function subtract(a, b) {
     return a - b;
-=======
-// subtract.js
-function subtract(a, b) {
-    // TODO return
->>>>>>> f60dd1e94defb2f01ea2fe22a1dada561c6d8522
 };
